753eda65f9325efb034f17dba0f58de7ba581f37
Weslley Dezidério <42120775+WeslleyDeziderio@users.noreply.github.com>
2019-07-24 23:14:13 -0300

    Update main.cpp

--- Empregado/main.cpp
+++ Empregado/main.cpp
@@ -6,7 +6,7 @@ int main(void){
     double salario;
 
     Empregado empregado1 = Empregado();
-    //Empregado *empregado2 = new Empregado();
+    Empregado *empregado2 = new Empregado("Weslley", "Deziderio", 5000);
 
     std::cout << "Insira nome e sobrenome do primeiro empregado: " << std::endl;
     std::cin >> nome >> sobrenome;
@@ -19,8 +19,15 @@ int main(void){
 
     std::cout << "O nome completo do primeiro empregado eh: " << empregado1.getNome() << " " << empregado1.getSobrenome() << std::endl;
     std::cout << "Salario do primeiro empregado: " << empregado1.getSalario() << "R$" << std::endl;
-
     std::cout << "O salario anual do empregado 1 eh: " << empregado1.getSalario()*12 << "R$" << std::endl;
-
+    std::cout << "O salario anual do empregado 1 com aumento de 10% eh: " << empregado1.getSalario()*12*1.1 << "R$" <<std::endl;
+    
+    std::cout << "O nome completo do segundo empregado eh: " << empregado2->getNome() << " " << empregado2->getSobrenome() << std::endl;
+    std::cout << "Salario do segundo empregado: " << empregado2->getSalario() << "R$" << std::endl;
+    std::cout << "O salario anual do empregado 2 eh: " << empregado2->getSalario()*12 << "R$" << std::endl;
+    std::cout << "O salario anual do empregado 2 com aumento de 10% eh: " << empregado2->getSalario()*12*1.1 << "R$" <<std::endl;
+    
+    delete empregado2;
+    
     return 0;
 }
